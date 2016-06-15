ce446d98aa585f518c5f123ab8d487def85042ab
Mark Gable <mgable43@gmail.com>
2016-06-15 09:34:33 -0700

    added SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2014-04-24 using generator-angular 0.8.0
+// Generated on 2016-02-10 using generator-angular 0.15.1
 'use strict';
 
 // # Globbing
@@ -7,48 +7,97 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
 
-    // Time how long tasks take. Can help when optimizing build times
-    require('time-grunt')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-    // Define the configuration for all the tasks
-    grunt.initConfig({
 
-        // Watches files for changes and runs tasks based on the changed files
-        watch: {
-            js: {
-                files: ['./scripts/*.js'],
-                tasks: ['jshint:all'],
-                options: {
-                    livereload: 35730 //true
-                }
-            }
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      sass: {
+          files: ['./styles/scss/*.scss'],
+          tasks: ['sass']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
         },
-
-        // Make sure code styles are up to par and there are no obvious mistakes
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'Gruntfile.js',
-                './scripts/*.js'
-            ]
-        }
-    });
-
-    grunt.registerTask('default',  function(){
-        return grunt.task.run(['lint']);
-    });
-
-    grunt.registerTask('lint', [
-        'watch',
-        'newer:jshint:all'
+        files: [
+          './{,*/}*.html',
+          './styles/{,*/}*.css',
+          './images/{,*/}*.{png,jpg,jpeg,gif}'
         ]
-    );
+      },
+      js: {
+        files: ['./*.js', './**/*.js'],
+        tasks: ['jshint:all']
+      }
+    },
+    jshint: {
+      options: {
+          jshintrc: '.jshintrc',
+          reporter: require('jshint-stylish')
+      },
+      all: [
+          'Gruntfile.js',
+          './scripts/*.js'
+      ]
+    },
+    sass: {
+        dist: {
+            files: [{
+                expand: true,
+                cwd: '.',
+                src: ['./styles/scss/*.scss'],
+                dest: './styles',
+                ext: '.css',
+                flatten: true
+            }]
+        }
+      },
+          // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          middleware: function (connect) {
+            return [
+              connect.static('.tmp'),
+
+              connect().use(
+                './styles',
+                connect.static('./styles')
+              ),
+              connect.static('./')
+            ];
+          }
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'sass',
+    'connect:livereload',
+    'watch'
+  ]);
+
+   grunt.registerTask('lint', [
+    'newer:jshint:all'
+    ]
+  );
+
 };
